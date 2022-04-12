63bc014319eb9b567da9e4340b2cd086fbf7d0cc
Paulien <r0810768@student.thomasmore.be>
2022-04-12 15:25:16 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { SuccessComponent } from './success/success.component';
-import { WarningComponent } from './warning/warning.component';
-import { ClicksComponent } from './clicks/clicks.component';
-import { ApesComponent } from './apes/apes.component';
-import { ApecasePipe } from './apecase.pipe';
 import { FolderComponent } from './folder/folder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SuccessComponent,
-    WarningComponent,
-    ClicksComponent,
-    ApesComponent,
-    ApecasePipe,
     FolderComponent,
   ],
   imports: [
