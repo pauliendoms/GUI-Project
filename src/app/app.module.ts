5c4d01a09b9978f8d7f972e3ea7bd637692afd3c
Paulien <r0810768@student.thomasmore.be>
2022-04-12 15:19:41 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SuccessComponent } from './success/success.component';
 import { WarningComponent } from './warning/warning.component';
+import { ClicksComponent } from './clicks/clicks.component';
+import { ApesComponent } from './apes/apes.component';
+import { ApecasePipe } from './apecase.pipe';
+import { FolderComponent } from './folder/folder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SuccessComponent,
     WarningComponent,
+    ClicksComponent,
+    ApesComponent,
+    ApecasePipe,
+    FolderComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/folder/folder.component.spec.ts
+++ src/app/folder/folder.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WarningComponent } from './warning.component';
+import { FolderComponent } from './folder.component';
 
-describe('WarningComponent', () => {
-  let component: WarningComponent;
-  let fixture: ComponentFixture<WarningComponent>;
+describe('FolderComponent', () => {
+  let component: FolderComponent;
+  let fixture: ComponentFixture<FolderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WarningComponent ]
+      declarations: [ FolderComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(WarningComponent);
+    fixture = TestBed.createComponent(FolderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
