bb59f01bc6b54f0fa9de6b4bac3ee2cdd2066bdd
Paulien <r0810768@student.thomasmore.be>
2022-05-24 20:50:34 +0200

    homepage gone and router updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { CardComponent } from './card/card.component';
 import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 import { FolderviewComponent } from './folderview/folderview.component';
-import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { QuizsettingsComponent } from './quizsettings/quizsettings.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { QuizCardComponent } from './quiz-card/quiz-card.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', redirectTo: 'cards', pathMatch: "full"},
   {path: 'cards', component: FolderviewComponent},
   {path: 'quiz', component: QuizviewComponent},
-  {path: 'cards/:id', component: CardsviewComponent}
+  {path: 'cards/:id', component: CardsviewComponent},
+  {path: '**', redirectTo: 'cards'}
 ]
 
 @NgModule({
@@ -32,7 +32,6 @@ const routes: Routes = [
     QuizviewComponent,
     CardComponent,
     FolderviewComponent,
-    HomeComponent,
     QuizsettingsComponent,
     QuizComponent,
     QuizCardComponent,
